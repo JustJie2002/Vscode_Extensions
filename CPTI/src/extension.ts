c2da667ee550e575f46df4960f9fc33a16a10021
Jie Chen <jiechen067@gmail.com>
2023-11-23 22:48:16 -0500

    Currently everything is working but it's only copying to clipboard

--- CPTI/src/extension.ts
+++ CPTI/src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fs from "fs";
+import path from "path";
+// const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,13 +16,43 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('CPTI.helloWorld', () => {
+	let foo = vscode.commands.registerCommand('CPTI.show_path', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Competitive Programming Template Inserter!');
+		var workspace_configs = vscode.workspace.getConfiguration();
+		var tpath = workspace_configs.get("cpti.path_to_template");
+
+		if (tpath == null || !fs.existsSync(tpath as string)) {
+			vscode.window.showInformationMessage("Please go to the setting to set the absolute to your CP templates.");
+			return;
+		}
+
+		var files = fs.readdirSync(tpath as string, {withFileTypes: true})
+		.filter(item => !item.isDirectory())
+		.map(file => file.name);
+		// .map(file => path.parse(file.name).name);
+		// console.log(files);
+		// files.forEach(file => {
+		// 	file = path.parse(file).name;
+		// 	return file;
+		// });
+		// console.log(files);
+
+		vscode.window.showQuickPick(files).then(selectedString => {
+			if (selectedString) {
+				const fileContent = fs.readFileSync(tpath + selectedString, 'utf-8');
+				vscode.env.clipboard.writeText(fileContent);
+				// // If a string is selected, show an input box
+				// vscode.window.showInputBox({
+				// 	placeHolder: `Enter value for ${selectedString}`
+				// }).then(userInput => {
+					
+				// });
+			}
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(foo);
 }
 
 // This method is called when your extension is deactivated
